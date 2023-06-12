23cc567357f6219dde5562fa6a2192b178193410
anusha-l23 <anusha.l@leanfolks.com>
2023-06-12 23:24:56 +0530

    router added

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,17 @@ import "../styles/App.css";
 
 import React, { Component } from "react";
 import Board from "./Board";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="Header">React Todo List App</div>
-
-        <Board />
+<BrowserRouter>
+<Routes>
+  <Route path="/" element={<Board />} exact />
+</Routes>
+</BrowserRouter>
       </div>
     );
   }
